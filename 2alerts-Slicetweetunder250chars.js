7cd372a0d5cdb366369264328937434894e8b5dd
was9419 <47761851+was9419@users.noreply.github.com>
2025-11-15 18:19:38 -0600

    Create 2alerts-Slicetweetunder250chars.js

---
+++ 2alerts-Slicetweetunder250chars.js
@@ -0,0 +1,6 @@
+var text = prompt("Please, enter any text in here:");
+var maxChars = 250
+var used = text.length
+var left = maxChars - text.length
+alert("You have written " + used + " characters out of " + maxChars + ". You have " + left + " left");
+alert(text.slice (0,250))
