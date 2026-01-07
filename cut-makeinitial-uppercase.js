68cbea9cab309656ad1b41049f32e396f6451634
was9419 <47761851+was9419@users.noreply.github.com>
2026-01-06 19:48:42 -0600

    Update cut-makeinitial-uppercase.js
    Make a few changes to make it work better

--- cut-makeinitial-uppercase.js
+++ cut-makeinitial-uppercase.js
@@ -1,5 +1,5 @@
 var name = prompt("What is your name?");
 var capital = name.slice (0,1);
-var name1 = name.slice (1,20);
+var name1 = name.slice (1,name.lenght);
 capital = capital.toUpperCase();
 alert("My name is: " + capital + name1);
