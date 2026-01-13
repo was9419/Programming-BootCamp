3ea1d359feddfe81699e58df8fc0a961b43c18e4
was9419 <47761851+was9419@users.noreply.github.com>
2026-01-12 20:16:39 -0600

    Create buymilkcalculator.js

---
+++ buymilkcalculator.js
@@ -0,0 +1,22 @@
+function getMilk(money) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  let bottles = money/1.5 
+  console.log("You can buy " + bottles + " with " + money + " dollars" );
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
