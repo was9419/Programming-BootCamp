2b09462e9657db4fce4edd8af22642940c39cb3f
was9419 <47761851+was9419@users.noreply.github.com>
2026-01-20 08:39:52 -0600

    Create lovecalcwithIFstatements.js

---
+++ lovecalcwithIFstatements.js
@@ -0,0 +1,11 @@
+prompt("What is your name?");
+prompt("What is your crush name?")
+var love = Math.random();
+love = love * 100;
+love = Math.floor(love) + 1;
+if (love > 70) {
+    alert("Your love percentage is: " + love + " %, You love eachother like Kanye loves Kanye");
+} else {
+alert("Your love percentage is: " + love + " %");    
+}
+
