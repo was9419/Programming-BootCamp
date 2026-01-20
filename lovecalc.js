1172ff5fa1ee260c794a01abe2ec63f2d89fd124
was9419 <47761851+was9419@users.noreply.github.com>
2026-01-19 18:54:14 -0600

    Create lovecalc.js

---
+++ lovecalc.js
@@ -0,0 +1,6 @@
+prompt("What is your name?");
+prompt("What is your crush name?")
+var love = Math.random();
+love = love * 100;
+love = Math.floor(love) + 1;
+alert("Your love percentage is: " + love);
