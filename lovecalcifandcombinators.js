1e765930e89eeb7ecb25dc9b17dd11dc140276c0
was9419 <47761851+was9419@users.noreply.github.com>
2026-01-20 08:58:31 -0600

    Create lovecalcifandcombinators.js

---
+++ lovecalcifandcombinators.js
@@ -0,0 +1,15 @@
+prompt("What is your name?");
+prompt("What is your crush name?")
+var love = Math.random();
+love = love * 100;
+love = Math.floor(love) + 1;
+if (love > 70) {
+    alert("Your love percentage is: " + love + " %, You love eachother like Kanye loves Kanye");
+} 
+if (love > 30 && love <= 70)
+    alert("Your love percentage is: " + love + " %");    
+}
+if (love <= 30)
+    alert("Your love percentage is: " + love + " %, You go together like water and oil.");    
+}
+
