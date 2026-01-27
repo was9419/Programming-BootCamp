c07d167405a094cebd9dd4c78a7ca2b4b0b539df
was9419 <47761851+was9419@users.noreply.github.com>
2026-01-26 19:01:27 -0600

    Create bmicalculatorifelsev2.js

---
+++ bmicalculatorifelsev2.js
@@ -0,0 +1,13 @@
+function bmiCalculator (weight, height) {
+    var interpretation = Math.round(weight / (height * height));
+
+    if (interpretation <= 18.5) { return("Your BMI is " + interpretation + ", so you are underweight."); 
+}
+
+    if (interpretation > 18.5 && interpretation <= 24.9) { return("Your BMI is " + interpretation + ", so you have a normal weight.");
+    } 
+    
+    if (interpretation > 24.9) { return("Your BMI is " + interpretation + ", so you are overweight."); }
+   
+    
+}
