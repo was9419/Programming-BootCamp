6ed61989af74cc2d6a94424610e1a3bf694f1ceb
was9419 <47761851+was9419@users.noreply.github.com>
2026-01-28 18:54:18 -0600

    Create guestList.js

---
+++ guestList.js
@@ -0,0 +1,6 @@
+var guestList = ["Wilfredo", "Odalys", "Samuel", "Salomon"];
+var name = prompt("What is your name?");
+if (guestList.includes (name)) { console.log("Welcome to the party!");
+                      } else  { 
+console.log("You are not invited");}
+
