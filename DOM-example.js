44eb70904e805d0b9ae771954d67d1cb52c468be
was9419 <47761851+was9419@users.noreply.github.com>
2026-07-16 19:03:21 -0600

    Manipulate last list item color and content with DOM and JS

---
+++ DOM-example.js
@@ -0,0 +1,16 @@
+var third = document.lastElementChild.lastElementChild.lastElementChild.lastElementChild;
+undefined
+document.lastElementChild.lastElementChild.lastElementChild.lastElementChild.color = "red";
+'red'
+document.lastElementChild.lastElementChild.lastElementChild.lastElementChild;
+<li class=​"list" style=​"color:​ red;​">​::marker​"Third"</li>​
+var third = document.lastElementChild.lastElementChild.lastElementChild.lastElementChild
+undefined
+third.style.color
+''
+third.style.color = "red"
+'red'
+third.inn
+undefined
+third.innerHTML = "Samuel";
+'Samuel'
